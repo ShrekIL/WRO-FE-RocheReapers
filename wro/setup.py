3f0a6195d27a213d69d231a51ea68ec70c5e17f6
Robin Kuettel <robin.kuettel@roche.com>
2025-04-02 12:40:39 +0200

    add ros configuration for wro node

---
+++ wro/setup.py
@@ -0,0 +1,28 @@
+import os
+from glob import glob
+from setuptools import find_packages, setup
+
+package_name = 'wro'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        #        ('share/ament_index/resource_index/packages'),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='ubuntu',
+    maintainer_email='1270161395@qq.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'images = wro.wro:main',
+        ],
+    },
+)
+
