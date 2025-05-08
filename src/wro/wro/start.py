a6eec29ea4c5c88ccb71618344175ce4ed24013b
Mischa <mischa.faessler@hotmail.com>
2025-05-08 16:15:35 +0200

    Add start skript

--- src/wro/wro/start.py
+++ src/wro/wro/start.py
@@ -1,18 +1,24 @@
-import RPi.GPIO as GPIO
+import lgpio
 import subprocess
 import os
 
-GPIO.setwarnings(False)
-GPIO.setmode(GPIO.BOARD)
-GPIO.setup(10, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+BUTTON_GPIO = 10
+
+h = lgpio.gpiochip_open(0)
+lgpio.set_mode(h, BUTTON_GPIO, lgpio.INPUT)
+lgpio.set_pull_up_down(h, BUTTON_GPIO, lgpio.PUD_DOWN)
 
 main_script = os.path.join(os.path.join(os.path.dirname(__file__)), "wro.py")
 stop_script = os.path.join(os.path.join(os.path.dirname(__file__)), "stop.py")
 
-while True:
-    if GPIO.input(10) == GPIO.HIGH:
-        subprocess.run(["python3", main_script])
-        break
-    else:
-        subprocess.run(["python3", stop_script])
-        break
+try:
+    while True:
+        if lgpio.gpio_read(h, BUTTON_GPIO) == 1:
+            subprocess.run(["python3", main_script])
+            break
+        else:
+            subprocess.run(["python3", stop_script])
+            break
+        #time.sleep(0.1)
+finally:
+    lgpio.gpiochip_close(h)
